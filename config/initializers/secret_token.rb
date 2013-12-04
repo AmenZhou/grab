7d63826104b0453661581bc346542c78765bf268
AmenZhou <chou.amen@gmail.com>
2013-12-03 19:05:24 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grab::Application.config.secret_key_base = 'cdef00678acddee27f79608d4cf57dd5d8d7d9f9fdf94ac37797cd2a09a4d228880fd98a602e59b27720bd0f0370a3432294b765835a66bdf154608de6dc56ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grab::Application.config.session_store :cookie_store, key: '_grab_session'
