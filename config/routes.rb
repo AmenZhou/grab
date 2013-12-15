144d2d1f676a0f8cab3fd1c2d95f11c4e8c9caa8
AmenZhou <chou.amen@gmail.com>
2013-12-15 15:52:25 -0500

    add home page and button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Grab::Application.routes.draw do
   resources :posts
+  root 'application#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
