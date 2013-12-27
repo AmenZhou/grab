67e9866deb4e72ca789c3bc7d130ea3430a14fe8
AmenZhou <chou.amen@gmail.com>
2013-12-27 16:16:12 -0500

    usa_life_online house lease post: add all types of lease

--- app/models/post_ulo_housrent.rb
+++ app/models/post_ulo_housrent.rb
@@ -4,24 +4,31 @@ class PostUloHousrent < ActiveRecord::Base
 	def PostUloHousrent::grab_ulo_housrent
 		page_array = [1]
 		region_array = [{:id => 1, :name => ''}]
-		fid_num = 247
+		fid_array = [{:id => 247, :name => 'Single Room'},
+						{:id => 246, :name => 'Whole House'},
+						{:id => 461, :name => 'Short Time'},
+						{:id => 394, :name => 'Else'},
+						{:id => 460, :name => 'Wanna Rent'}]
 		arr_list = Array.new
 		#grab page data , put into arr_list
 		page_array.each do |page|
 			region_array.each do |region|
-				doc = Nokogiri::HTML(open("http://www.usalifeonline.com/f/list.php?fid=" + fid_num.to_s + "&" + "city_id=" + region[:id].to_s + "&" + "page=" + page.to_s))
-				doc.css('table[class="listtable"]').each do |order|
-					unit = Hash.new
-					unit[:detail_url] = order.css('a').attr('href').value
-					unit[:unique_code] = unit[:detail_url][/\d{6}/]
-					unit[:title] = order.css('a').attr('title').value
-					arr_list << unit					
+				fid_array.each do |fid|
+					doc = Nokogiri::HTML(open("http://www.usalifeonline.com/f/list.php?fid=" + fid[:id].to_s + "&" + "city_id=" + region[:id].to_s + "&" + "page=" + page.to_s))
+					doc.css('table[class="listtable"]').each do |order|
+						unit = Hash.new
+						unit[:detail_url] = order.css('a').attr('href').value
+						unit[:unique_code] = unit[:detail_url][/\d{6}/]
+						unit[:title] = order.css('a').attr('title').value
+						unit[:ct_name] = fid[:name]
+						arr_list << unit
+					end
 				end
 			end
 		end
 		#take unique code from database
 		code_list = PostUloHousrent.where(:site_source => 'ulo').map(&:unique_code)
-		arr_list.each do |unit|
+		arr_list.each do |unit|											
 			#compare unique codes, if one line has been grabbed, avoid grabbing its detail page again
 			unless code_list.include? unit[:unique_code]
 				post = PostUloHousrent.new
@@ -38,12 +45,21 @@ class PostUloHousrent < ActiveRecord::Base
 				
 				#grab all content area code
 				grab_content_phone = doc.css('td[class="showinfo"]').css('td[width="89%"]')
+				#error condition solve
+				if grab_content_phone.empty?
+					break
+				end
 				content = grab_content_phone[1].text.strip
 				phone_n = grab_content_phone[2].text.strip
 				#grab all date area code
 				grab_date = doc.css('td[class="showinfo"]').css('td[width="50%"]')
+				#error condition solve
+				if grab_date.empty?
+					break
+				end
 				#select date from the date area code
 				up_time = grab_date[0].text[/\d{4}.?\d{2}.?\d{2}/].strip
+				
 				#give value to database
 				post.content = content
 				post.title = unit[:title]
@@ -54,6 +70,7 @@ class PostUloHousrent < ActiveRecord::Base
 				post.phone_n = phone_n
 				post.rent_m = content[/\$\d+/]
 				post.site_source = "ulo"
+				post.ct_name = unit[:ct_name]
 				post.save
 			end
 		end
