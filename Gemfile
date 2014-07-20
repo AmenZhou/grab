54808d2610ff4400d74af791b71d23a5d254393e
Ken Lu/HaiMeng <chou.amen@gmail.com>
2014-07-20 21:56:54 +0000

    understand offset and update database

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
+gem 'mysql2'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
@@ -42,8 +43,10 @@ end
 # gem 'capistrano', group: :development
 
 # Use debugger
-gem 'debugger', group: [:development, :test]
+#gem 'debugger', group: [:development, :test]
 gem 'nokogiri'
+gem 'yelp'
 gem 'rails_12factor', group: :production
+gem 'byebug'
 gem 'pg', group: :production
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: 'master'
--- app/models/yelp_grab.rb
+++ app/models/yelp_grab.rb
@@ -2,16 +2,16 @@ require_relative '../../lib/yelp'
 class YelpGrab < ActiveRecord::Base
   def YelpGrab::grab
     begin_time = Time.now
-    (1..900).each do |i|
-      rs = Yelp.client.search('NYC', {term: 'Nail salons', limit: 20, offset: i})
+    (21..50).each do |i|
+      rs = Yelp.client.search('NYC', {term: 'Nail salons', limit: 20, offset: (i * 20)})
 
       rs.businesses.each do |business|
         yp = YelpGrab.find_or_create_by_yelp_id(business.id)
         yp.update_attributes(
-          name: business.try(:name),
-          phone_num: business.try(:display_phone),
-          url: business.try(:url),
-          address: business.try(:location).try(:display_address).try(:join, ' ')
+          name: business.try('name'),
+          phone_num: business.try('display_phone'),
+          url: business.try('url'),
+          address: business.try('location').try('display_address').try('join', ' ')
         )
       end
     end
@@ -22,7 +22,7 @@ class YelpGrab < ActiveRecord::Base
   def YelpGrab::export
     File.open('public/nyc.csv', 'w') do |file|
       YelpGrab.all.each do |yp|
-        file.puts "#{yp.name}, #{yp.phone_num}, #{yp.url}, #{yp.address}"
+        file.puts "#{yp.name} | #{yp.phone_num} | #{yp.url} | #{yp.address}"
       end
     end
   end
